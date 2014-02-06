db3a16c1d05eda8e44e71f12f2961d641f801521
Chris Dart <technology@fsmn.org>
2014-02-06 11:30:24 -0600

    Good. Grief!

--- application/views/order/totals.php
+++ application/views/order/totals.php
@@ -14,7 +14,7 @@ foreach($orders as $order){
 			$orderLines[] = "<tr><td colspan='7' class='totals'>Order Total: " . getAsCash($poTotal). "</td></tr>";
 
 		}
-		$line = sprintf("<tr><td colspan='7' class='poLine'><a href='%s'>%s</a></td></tr>",site_url("order/view/$order->kPo"),$order->kPo);
+		$line = sprintf("<tr><td colspan='7' class='poLine'><a href='%s'>%s</a></td></tr>",site_url("order/view/$order->kPO"),$order->kPO);
 		$orderLines[] = $line;
 		$lastPO = $order->kPO;
 		$notFirst = true;
