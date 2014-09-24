30bd1a3af2d50e5011d51c2010ed92ca950c4f78
Chris Dart <technology@fsmn.org>
2014-09-24 17:39:23 -0500

    Making the order system more mobile-friendly

--- application/views/vendor/list.php
+++ application/views/vendor/list.php
@@ -3,7 +3,7 @@
 foreach($vendors as $vendor){
 	$output[] = "<fieldset class='vendor-list'><legend>$vendor->vendorName</legend>";
 	$output[] = "<p><span class='button edit edit_vendor' id='edit_$vendor->kVendor'>Edit</span>&nbsp;";
-	$output[] = "<span class='button view_orders' id='view_$vendor->kVendor'>Orders</span>&nbsp;";
+	$output[] = sprintf("<a class='button' href='%s'>Orders</a>",site_url("order/vendor_orders/$vendor->kVendor"));
 	$output[] = "<span class='new_order new button' id='new_$vendor->kVendor'>New Order</span></p>";
 	$output[] = "</fieldset>";
 }
